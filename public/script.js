723646df678c8b6b66a4980efb880b42ced0e2ee
Mislam23 <islam.mohammad.student@gmail.com>
2022-05-21 15:19:39 -0400

    broadcast bug fixed and now multiple users can join video call

--- public/script.js
+++ public/script.js
@@ -1,9 +1,18 @@
 //this file holds all the client side code
-let myVideoStream;
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 
+//this keeps the video muted initially
 myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
+let myVideoStream;
 navigator.mediaDevices.getUserMedia({
   audio: true,
   video: true,
@@ -12,6 +21,33 @@ navigator.mediaDevices.getUserMedia({
   myVideoStream = stream;
   //here we place the add Video Stream function
   addVideoStream(myVideo, stream);
+
+  //we're adding another video element when the user joins our call room
+  peer.on("call", (call) => {
+    call.answer(stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+      addVideoStream(video, userVideoStream);
+    });
+  });
+
+  //when the user is connected call the connect to new user function
+  socket.on("user-connected", (userId) => {
+    connectToNewUser(userId, stream);
+  });
+});
+
+//at this point the functions keep calling other functions within them to establish a call function that is seen on the UI
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+};
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
 });
 
 //add Video Stream function will add our video to the video element
--- server.js
+++ server.js
@@ -2,15 +2,30 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-//uuid generates a unique url that will be used for each room
+//uuid below generates a unique url that will be used for each room
 const { v4: uuidv4 } = require("uuid");
+
+//using ejs in express
+app.set('view engine', 'ejs')
+
+//socket.io lets us do live communication
+const io = require("socket.io")(server);
+
+//PeerJS allows the implementation of WebRTC
+//WebRTC allows websites to stream media such as video/audio
+const { ExpressPeerServer } = require("peer");
+
+//peer server function
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+//calling the peer server function
+app.use("/peerjs", peerServer);
 
 //code below will allow access to stle.css and script.js in public folder 
 //as in allow access to all static files in the public folder
 app.use(express.static('public'));
-
-//using ejs in express
-app.set('view engine', 'ejs')
 
 //below will create a unique url id for each room
 app.get("/", (req, res) => {
@@ -32,5 +47,12 @@ app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+//the code below takes in the roomId and userId and establishes a connection to both users 
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+  });
+});
 
 server.listen(3030);
